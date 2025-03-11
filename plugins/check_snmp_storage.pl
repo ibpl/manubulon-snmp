ibpl/manubulon-snmp

commit 35e9ec4c75fc28a8c7955bb7b43c5cc3e185aeda

Fix get_table() call hangups with docker
check_snmp_storage.pl executions may hang (checked on Debian 11/12)
while querying host with docker filesystems with many long filesystem
mountpoints like

```
iso.3.6.1.2.1.25.2.3.1.3.1 = STRING: "Physical memory"
iso.3.6.1.2.1.25.2.3.1.3.3 = STRING: "Virtual memory"
iso.3.6.1.2.1.25.2.3.1.3.6 = STRING: "Memory buffers"
iso.3.6.1.2.1.25.2.3.1.3.7 = STRING: "Cached memory"
iso.3.6.1.2.1.25.2.3.1.3.8 = STRING: "Shared memory"
iso.3.6.1.2.1.25.2.3.1.3.10 = STRING: "Swap space"
iso.3.6.1.2.1.25.2.3.1.3.35 = STRING: "/run"
iso.3.6.1.2.1.25.2.3.1.3.36 = STRING: "/"
iso.3.6.1.2.1.25.2.3.1.3.38 = STRING: "/dev/shm"
iso.3.6.1.2.1.25.2.3.1.3.39 = STRING: "/run/lock"
iso.3.6.1.2.1.25.2.3.1.3.40 = STRING: "/sys/fs/cgroup"
iso.3.6.1.2.1.25.2.3.1.3.59 = STRING: "/test1"
iso.3.6.1.2.1.25.2.3.1.3.60 = STRING: "/test2"
iso.3.6.1.2.1.25.2.3.1.3.61 = STRING: "/test3"
iso.3.6.1.2.1.25.2.3.1.3.64 = STRING: "/var/lib/docker/containers/43b4ba50f14279701a4d13333e00a26375aa114b9100d845044107cbcd5ce4a0/mounts/shm"
iso.3.6.1.2.1.25.2.3.1.3.66 = STRING: "/var/lib/docker/containers/5f62f6600a81d7c8b116a134853d9b445711d3a9e79935ded1831c09cab5d6176/mounts/shm"
iso.3.6.1.2.1.25.2.3.1.3.68 = STRING: "/var/lib/docker/containers/7ddef6926988082e6c5d33bdf8544cf7659f85e760587111c3737f2addc04d60a/mounts/shm"
[...]
```
It was tested that issue does not occur if `get-next-requests` is used instead of
`get-bulk-requests`. This fix forces `get-next-requests` to be used in problematic call.

Related: https://metacpan.org/pod/Net::SNMP#get_table()-retrieve-a-table-from-the-remote-agent
Author-Change-Id: IB#1089430

plugins/check_snmp_storage.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl -w
 ############################## check_snmp_storage ##############
-# Version : 2.1.0
-# Date :  Jun 1 2007
-# Author  : Patrick Proy ( patrick at proy.org)
+# Version : 2.2.0
+# Date : Mar 11 2015
+# Author : Patrick Proy ( patrick at proy.org)
+#          Copyright (C) 2020 Informatyka Boguslawski sp. z o.o. sp.k., https://www.ib.pl/
 # Help : http://nagios.manubulon.com
 # License : GPL - http://www.fsf.org/licenses/gpl.txt
 # TODO :
@@ -595,7 +596,7 @@ if (defined($o_index)) {
     if (version->parse(Net::SNMP->VERSION) < 4) {
         $resultat = $session->get_table($descr_table);
     } else {
-        $resultat = $session->get_table(Baseoid => $descr_table);
+        $resultat = $session->get_table(Baseoid => $descr_table, maxrepetitions => 1);
     }
 }
 

commit 5a4b93622718be6b98e6cb703719b60f0b854086

Fix modification year number
Fixes: 35e9ec4c75fc28a8c7955bb7b43c5cc3e185aeda
Author-Change-Id: IB#1089430

plugins/check_snmp_storage.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 ############################## check_snmp_storage ##############
 # Version : 2.2.0
-# Date : Mar 11 2015
+# Date : Mar 11 2025
 # Author : Patrick Proy ( patrick at proy.org)
 #          Copyright (C) 2020 Informatyka Boguslawski sp. z o.o. sp.k., https://www.ib.pl/
 # Help : http://nagios.manubulon.com